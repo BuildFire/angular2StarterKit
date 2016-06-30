3dd44473676fa9caf138f6228ff36aebaf62ac12
attila buturla <abuturla@buildfire.com>
2016-06-30 10:48:56 -0700

    recovered files

---
+++ widget/src/app.component.ts
@@ -0,0 +1,72 @@
+import {Component, View, OnInit, Input} from 'angular2/core';
+import {COMMON_DIRECTIVES} from 'angular2/common';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './src/app.html'
+})
+
+export class AppComponent  {
+    todos: Todo[];
+    toggleMode: boolean = true;
+
+    constructor() {
+        this.todos = [
+            new Todo('Get Milk', false),
+            new Todo('Brush Teeth', false)
+        ];
+    }
+
+    addTodo(newTodo){
+        this.todos.unshift(new Todo(newTodo, false));
+    }
+    toggleTodoStatus(name: string){
+        this.findToDo(name)
+            .toggleStatus();
+    }
+    deleteTodo(name: string){
+        let todo = this.findToDo(name);
+        let index = this.todos.indexOf(todo);
+        this.todos.splice(index, 1);
+    }
+
+    findToDo(name: string){
+        var todo = this.todos.find((todo) => {
+            return todo.name === name;
+        });
+
+        return todo;
+    }
+    
+    getCount(isCompleted: boolean){
+        let todos = this.todos.find((todo) => {
+            return todo.isCompleted === isCompleted;
+        });
+
+        let count = (todos && todos.length) ? todos.length : 0;
+
+        return count;
+    }
+
+    toggleAll(){
+        this.todos.map((todo) => {
+            return todo.isCompleted = this.toggleMode;
+        });
+
+        this.toggleMode = !(this.toggleMode);
+    }
+}
+
+export class Todo {
+    name: string;
+    isCompleted: boolean = false;
+
+    constructor(name: string, isCompleted: boolean){
+        this.name = name;
+        this.isCompleted = isCompleted;
+    }
+
+    toggleStatus(){
+        this.isCompleted = !(this.isCompleted);
+    }
+}
